d8e90a8e50db19fdc8e73c87704f8912d6fb83c4
Matiashartwig23 <57971633+Matiashartwig23@users.noreply.github.com>
2019-12-10 20:11:57 -0800

    new

---
+++ Calculator/calculator.js
@@ -0,0 +1,31 @@
+var stack = [];
+
+var screen = getElementById("result");
+
+function clear() {
+
+}
+
+function enter() {
+
+}
+
+function numberHandle() {
+
+}
+
+function operatorHandle() {
+
+}
+
+function decimalHand() {
+
+}
+
+function calculate() {
+
+}
+
+document.addEventListener("click"),function () 
+        document.write()
+    }
